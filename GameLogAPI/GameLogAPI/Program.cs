f879cdbba3d8bc8a2cf8deababf4d28a3915c63d
DownyDowner <boris.vancauter@gmail.com>
2025-06-06 09:17:10 +0200

    refactor(Program.cs): improve readability

--- GameLogAPI/GameLogAPI/Program.cs
+++ GameLogAPI/GameLogAPI/Program.cs
@@ -16,46 +16,46 @@ var builder = WebApplication.CreateBuilder();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 var signingKey = Environment.GetEnvironmentVariable("JWT_SIGNING_KEY");
 
+builder.Services.AddDbContext<GameDbContext>(options =>
+    options.UseSqlite(connectionString));
 
+// Identity setup
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<GameDbContext>()
     .AddDefaultTokenProviders();
 
+// Configure FastEndpoints with JWT
 builder.Services
-   .AddAuthenticationJwtBearer(s => s.SigningKey = signingKey)
-   .AddAuthorization()
-   .AddFastEndpoints()
-   .AddFastEndpoints()
-   .SwaggerDocument();
+    .AddFastEndpoints()
+    .AddAuthenticationJwtBearer(s => s.SigningKey = signingKey)
+    .AddAuthorization()
+    .SwaggerDocument();
 
-builder.Services.AddDbContext<GameDbContext>(options =>
-    options.UseSqlite(connectionString));
-   
+// CORS policy
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
 builder.Services.AddCors(options => {
     options.AddPolicy(MyAllowSpecificOrigins,
-        builder => builder.WithOrigins("http://localhost:5173")
-                          .AllowAnyHeader()
-                          .AllowAnyMethod());
+        policy => policy.WithOrigins("http://localhost:5173")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
 });
 
+// Exception handling
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 
+// Services & repositories
 builder.Services.AddScoped<AuthService>();
-
 builder.Services.AddScoped<IGameRepository, GameRepository>();
 builder.Services.AddScoped<GameService>();
-
 builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
 builder.Services.AddScoped<PlatformService>();
 
 var app = builder.Build();
 
+// Seed role
 using (var scope = app.Services.CreateScope()) {
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
     var roleName = RoleConstants.ADMIN;
 
     if (!await roleManager.RoleExistsAsync(roleName)) {
@@ -63,13 +63,14 @@ using (var scope = app.Services.CreateScope()) {
     }
 }
 
-app.UseAuthentication()
-   .UseAuthorization()
-   .UseFastEndpoints()
-   .UseSwaggerGen();
-
+// Middleware pipeline
 app.UseCors(MyAllowSpecificOrigins);
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseExceptionHandler();
+app.UseFastEndpoints();
+app.UseSwaggerGen();
 
 app.Run();
